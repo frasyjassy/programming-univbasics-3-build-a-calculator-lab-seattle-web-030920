4ae693f4133e0875d2c14b9d39a78715b346b535
JC Ramirez <jsmnramirez25@gmail.com>
2020-04-06 01:42:41 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -13,13 +13,13 @@ def division(num1, num2)
 end
 
 def multiplication(num1, num2)
-  num1 * num2
+  p num1 * num2
 end
 
 def modulo(num1, num2)
-  num2 % num1
+  p num2 % num1
 end
 
 def square_root(num)
-   
+  p num**
 end
